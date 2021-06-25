5ce1fc3590b86b64cb3d8394fca63cf3f20f5ce9
Jean Domigues <jeandomingues752@gmail.com>
2021-06-24 23:00:12 -0300

    Criando contexto de auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import { Home } from "./pages/Home";
-import { NewRoom } from './pages/NewRoom';
-
+import { useState } from 'react';
+import { createContext } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
+import firebase from 'firebase';
+import { auth } from './services/firebase';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+export const AuthContext = createContext({} as any);
 
 function App() {
+  const [user, setUser] = useState();
+
+  async function sigInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Usuário sem informações suficientes');
+        }
+
+        setUser({
+          id: uid, 
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    });
+  }
+
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContext.Provider value={{ user, sigInWithGoogle }}>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,29 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import { auth, firebase } from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logo from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+
 
 import { Button } from '../components/Buttton';
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+      
+    });
+
+    // history.push('/rooms/new')
+  }
 
   return (
     <div id='page-auth'>
@@ -20,7 +35,7 @@ export function Home() {
       <main>
         <div className='main-content'>
           <img src={logo} alt='LetMeAsk' />
-          <button onClick={() => history.push('/rooms/new')} className='create-room'>
+          <button onClick={handleCreateRoom} className='create-room'>
             <img src={googleIconImg} alt='Google' />
             Crie sua sala com o Google
           </button>
