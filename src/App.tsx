095fde7237142c31a415660fa63bcdea0c19a596
Jean Domigues <jeandomingues752@gmail.com>
2021-07-08 21:48:49 -0300

    Tirando complexidade do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,17 @@
-import { useState } from 'react';
-import { createContext } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
-
-import firebase from 'firebase';
-import { auth } from './services/firebase';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-};
-
-type AuthContextType = {
-  user: User | undefined;
-  sigInWithGoogle: () => void;
-};
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
-  const [user, setUser] = useState<User>();
-
-  async function sigInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);
-
-    if (result.user) {
-      const { displayName, photoURL, uid } = result.user;
-
-      if (!displayName || !photoURL) {
-        throw new Error('Usuário sem informações suficientes');
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL,
-      });
-    }
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, sigInWithGoogle }}>
+      <AuthContextProvider>
         <Route path='/' exact component={Home} />
         <Route path='/rooms/new' component={NewRoom} />
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
