e7c871495841b12174ac6fef6eac5090f1e3e2d1
Jean Domigues <jeandomingues752@gmail.com>
2021-07-12 21:23:58 -0300

    Criando a página da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,15 +1,25 @@
-import logoImg from '../assets/images/logo.svg';
-import { Button } from '../components/Buttton';
+import { useParams } from 'react-router';
 
-import '../styles/room.scss'
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Buttton';
+import { RoomCode } from '../components/RoomCode';
+
+import '../styles/room.scss';
+
+type RoomParams = {
+  id: string;
+}
 
 export function Room() {
+  const params = useParams<RoomParams>();
+
   return (
     <div id='page-room'>
       <header>
         <div className='content'>
           <img src={logoImg} alt='LetMeAsk' />
-          <div>Código</div>
+          <RoomCode code={params.id} />
         </div>
       </header>
 
